b5c688306a3b444343a78756c438bfb3e6b924a4
Danielj2005 <dbarrueta42@gmail.com>
2025-08-05 14:50:46 -0400

    load of index.html to the dollarApi

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const https = require('https'); // <--- Añade esta línea
+const path = require('path'); // <--- Añade esta línea
 
 const app = express();
 const port = 3000;
